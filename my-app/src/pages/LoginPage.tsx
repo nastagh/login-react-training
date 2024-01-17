54f4dad364691275ea18f269b04c53a7aa1ca7fc
Anastasiya <nastagh96@mail.ru>
2024-01-17 23:41:40 +0100

    added some styles

--- my-app/src/pages/LoginPage.tsx
+++ my-app/src/pages/LoginPage.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import "../styles/loginPage.scss";
 
+
+
+enum InputName {
+  Email = 'Email',
+  Password = 'Password',
+  Agreement = 'Agreement',
+  Login = 'Login'
+}
+
 const LoginPage = () => {
   return (
     <>
@@ -9,47 +18,47 @@ const LoginPage = () => {
           <h1 className="login-container__title">
             <strong>login</strong>
           </h1>
-          <form>
-            <div className="email-container">
-              <label>
-                <img alt="email" title="email" />
-                <input name="email" type="email" id="email" placeholder="Email"></input>
+          <form className="login-container__form">
+            <div>
+              <label className="input-container input-container_style">
+                <img alt={InputName.Email} title={InputName.Email} src={'../assets/images/letter.png'} className="input-img"/>
+                <input name={InputName.Email} type="email" id={InputName.Email} placeholder={InputName.Email}></input>
               </label>
             </div>
 
-            <div className="password-container">
-              <label>
-                <img alt="password" title="password" />
-                <input name="password" type="password" id="password" placeholder="Password"></input>
+            <div>
+              <label className="input-container input-container_style">
+                <img alt={InputName.Password} title={InputName.Password} src={'../assets/images/lock.png'} className="input-img"/>
+                <input name={InputName.Password} type="password" id={InputName.Password} placeholder={InputName.Password}></input>
               </label>
             </div>
 
-            <div className="agreements-container">
-              <label>
-                <input name="agreements" type="checkbox"></input>
+            <div>
+              <label className="input-container agreements-container">
+                <input name={InputName.Agreement} type="checkbox"></input>
                 <span>Remember me</span>
               </label>
             </div>
-            <div>
-              <input name="login" type="button" value={'login'}></input>
+            <div className="input-container login-button">
+              <input name={InputName.Login} type="button" value={InputName.Login}></input>
             </div>
           </form>
-          <div>
+          <div className='login-bottom'>
             <div>
               Or login with
             </div>
-            <div>
-              <div>
-                <img alt=""></img>
+            <div className='alternative-text-container'>
+              <div className='alternative-text'>
+                <img alt="Facebook" title="Facebook" src={'../assets/images/facebook.png'} className="alternative-img"></img>
                 <span>Facebook</span>
               </div>
-              <div>
-                <img alt=""></img>
+              <div className='alternative-text'>
+                <img alt="Google" title="Google" src={'../assets/images/google.png'} className="alternative-img"></img>
                 <span>Google</span>
               </div>
             </div>
           </div>
-          <div>
+          <div className="sign-up-container">
             Not a member? <a href="https://developer.mozilla.org">Sing up now</a> 
           </div>
         </div>
